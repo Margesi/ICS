ccc41c318c6a787de35b12114f0249c2ae1051ee
marges <pinderimargesi@gmail.com>
2021-05-10 19:44:11 +0200

    c2

--- GroupingbyRegions.R
+++ GroupingbyRegions.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 #Feel free to suggest other stuff to summarize
 p20<-as.data.frame(ics2020) %>%
   group_by(Region) %>% summarise(mean_fertility=mean(Total.Fertility.Rate),mean_LifeExpectancy=mean(Life.Expectancy.at.Birth..Both.Sexes),median_Fertility=median(Total.Fertility.Rate),median_LifeExpectancy=median(Life.Expectancy.at.Birth..Both.Sexes) ,sd_fertility=sd(Total.Fertility.Rate),sd_LifeExpectancy=sd(Life.Expectancy.at.Birth..Both.Sexes))
@@ -6,7 +6,7 @@ p21<-as.data.frame(ics2020) %>%
   group_by(Subregion) %>% summarise(mean_fertility=mean(Total.Fertility.Rate),mean_LifeExpectancy=mean(Life.Expectancy.at.Birth..Both.Sexes),median_Fertility=median(Total.Fertility.Rate),median_LifeExpectancy=median(Life.Expectancy.at.Birth..Both.Sexes) ,sd_fertility=sd(Total.Fertility.Rate),sd_LifeExpectancy=sd(Life.Expectancy.at.Birth..Both.Sexes))
 
 ##Text Output
-formattable(p20,type="text") 
+t1<-formattable(p20,type="text") 
 formattable(p21,type="text")
 
 #Latex Output
